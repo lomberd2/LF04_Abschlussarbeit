f124d65dd9dc8d79dd20dc18592360e4c29ea819
lomberd2 <joeyblankenhagen@gmail.com>
2022-06-15 09:09:20 +0200

    Init

---
+++ js/lieferkostenKalkulation.js
@@ -0,0 +1,9 @@
+function lkRechnung() {
+    const warenwertInput = document.getElementById("warenwertInput");
+    const plzGebietSelect = document.getElementById("plzGebietInput");
+    const expressCheckbox = document.getElementById("expressInput");
+    const lieferkostenOutput = document.getElementById("lieferkostenOutput");
+
+
+
+}
